dc6eeb9a6f1f9233e64ca4e26419ca5b8c420723
Gustavo Morais <gustavopguto@gmail.com>
2019-08-14 23:11:23 -0300

    Adding Properties Component
    Building form according to clicked endpoint and operation

---
+++ console-frontend/src/components/hydra-console/properties-editor/PropertiesEditor.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import { withStyles } from '@material-ui/styles';
+import Input from '@material-ui/core/Input';
+import Grid from '@material-ui/core/Grid';
+import GuiTheme from './../../../app/gui-theme';
+
+const styles = theme => ({
+    propertyInput: {
+        color: GuiTheme.palette.primary.dark,
+        marginLeft: '10px',
+        marginRight: '6px',
+    },
+    propertyContainer: {
+        marginTop: '2px',
+        marginBottom: '2px'
+    },
+    input: {
+        flex: '100'
+    }
+});
+
+class PropertiesEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.classesProperties = [];
+        this.filledProperties = [];
+    }
+    
+    selectButton(clickedButton){
+        var updatedButtons = this.state.buttons.slice();
+        updatedButtons[this.state.selectedButton] = false;
+        updatedButtons[clickedButton] = true;
+        this.setState({
+            buttons: updatedButtons,
+            selectedButton: clickedButton
+        })
+         
+    }
+
+    generateField(propertyName, placeholder = null){
+        const { classes } = this.props;
+        //this.filledProperties[fieldName];
+        return (
+        <Grid
+            className={classes.propertyContainer}
+            container
+            direction="row"
+            justify="flex-start"
+            alignItems="center">
+            <label className={classes.propertyInput}> 
+                {propertyName}:
+            </label>
+            <Input
+                placeholder={placeholder}
+                name={propertyName}
+                value={this.props.properties[propertyName]}
+                onChange={this.props.onChange}
+                className={classes.input}
+                inputProps={{
+                    'aria-label': 'description',
+                }}
+            />
+        </Grid>)
+    }
+
+    generateProperties(){
+        var fields = []
+    
+        for(var property in this.props.properties){
+            fields.push(this.generateField(property, null));
+        }
+        return fields;
+    }
+
+    render() {
+        return this.generateProperties()
+    }
+}
+
+export default withStyles(styles)(PropertiesEditor);
