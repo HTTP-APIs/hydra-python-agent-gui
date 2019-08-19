7d464e9d8b3b2dfc77b4fece7762acdc378f7925
Gustavo Morais <gustavopguto@gmail.com>
2019-08-18 23:03:47 -0300

    Adding Server URL Input to enable server switching

--- console-frontend/src/app/app.js
+++ console-frontend/src/app/app.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import NavBar from '../components/navbar/NavBar'
+import NavBar from '../components/navbar/NavBar';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
+import { withStyles } from '@material-ui/styles';
+import Grid from '@material-ui/core/Grid';
+import Send from '@material-ui/icons/Send';
 import HydraConsole from '../components/hydra-console/HydraConsole'
 import HydraGraph from '../components/hydra-graph/HydraGraph'
-import Grid from '@material-ui/core/Grid';
 import './app.scss';
 import GuiTheme from './gui-theme'
 import { ThemeProvider } from '@material-ui/styles';
 import axios from 'axios';
+
+const styles = theme => ({
+  serverInputContainer: {
+    width: '100%',
+    backgroundColor: GuiTheme.palette.primary.light,
+  },
+  serverInput: {
+      width: '93%'
+  },
+});
 
 class AgentGUI extends React.Component {
   constructor(props) {
@@ -15,24 +31,31 @@ class AgentGUI extends React.Component {
       consoleWidth: 6, //6 | 12 
       hidden: false, //false | true,
       classes: null,
+      apidocGraph: {edges: null, nodes: null},
+      serverURL: "http://localhost:8080/serverapi/"
     }
 
+    this.agentEndpoint = "http://localhost:5000"
   }
   
   componentDidMount() {
-    axios.get('http://localhost:5000/hydra-doc')
+    axios.get(this.agentEndpoint + "/hydra-doc")
       .then(res => {
-        var endpoints = null;
-        var classes = null;
-
         this.setState({
           //for this.supportedClass > if @id="vocab:EntryPoint" then supportedProperty.property.labe
           classes: res.data.supportedClass,
-        })
-        console.log(res);
+          serverURL: res.data.serverURL.replace(/\/$/, "") + "/"
+        }, () => this.render())
+      });
+
+      axios.get(this.agentEndpoint + "/apidoc-graph")
+      .then(res => {
+        this.setState({
+          //for this.supportedClass > if @id="vocab:EntryPoint" then supportedProperty.property.labe
+          apidocGraph: res.data
+        }, () => this.render())
       });
   }
-
 
   toggleGraph(){
     if(this.state.hidden){
@@ -64,23 +87,56 @@ class AgentGUI extends React.Component {
               <NavBar text="Hydra API" fontSize='1.3em'
                 backgroundColor={GuiTheme.palette.primary.light}
                 fontColor="textSecondary"></NavBar>
-              <HydraGraph></HydraGraph>
+                <Grid
+                  container
+                  direction="row"
+                  justify="center"
+                  alignItems="center"
+                  className={classes.serverInputContainer}>
+                  <InputLabel htmlFor="aaaaa">Server URL:</InputLabel>
+                  <Input
+                      id="aaaaa"
+                      placeholder="Server URL - Default: https://localhost:8080/serverapi/"
+                      onKeyPress={ (e) => {if(e.key === 'Enter'){ this.submitServerURL(e) } }}
+                      value={this.state.serverURL}
+                      onChange={ (e) => this.handleChangeServerURL(e) }
+                      className={classes.serverInput}
+                      inputProps={{
+                          'aria-label': 'hydrus-url',
+                      }}
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={(e) => this.submitServerURL(e) }
+                          >
+                            <Send/>
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                  />
+              </Grid>
+              <HydraGraph
+                apidocGraph={this.state.apidocGraph}>
+              </HydraGraph>
             </Grid>
     
             <Grid item md={this.state.consoleWidth} xs={12} color='primary'>
               <NavBar text="Agent Console" fontSize='1.3em'
                 backgroundColor={GuiTheme.palette.primary.dark}
               ></NavBar> 
-              <HydraConsole  hydraClasses={this.state.classes} color='primary' ></HydraConsole>
+              <HydraConsole
+                serverUrl={this.state.serverURL}
+                hydraClasses={this.state.classes}
+                color='primary' ></HydraConsole>
             </Grid>
           </Grid>
         </ThemeProvider>
       );
     }else{
-      return "Fetching Hydra API Doc from the Server..."
+      
+      return (<div className="lds-circle"><div></div></div>)
     }
   }
 }
-
-export default AgentGUI;
-
+export default withStyles(styles)(AgentGUI);
