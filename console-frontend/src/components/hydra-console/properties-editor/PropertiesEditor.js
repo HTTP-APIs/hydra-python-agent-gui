e5fa777ad6c1f154cb22d68ce5181165dde1eead
Priyanshu Nayan <priyanshunayan@Priyanshus-MacBook-Air.local>
2020-05-29 04:36:23 -0700

    jsx to js beacuse facebook recommends

--- console-frontend/src/components/hydra-console/properties-editor/PropertiesEditor.js
+++ console-frontend/src/components/hydra-console/properties-editor/PropertiesEditor.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { withStyles } from '@material-ui/styles';
 import Input from '@material-ui/core/Input';
 import Grid from '@material-ui/core/Grid';
-import GuiTheme from './../../../app/gui-theme';
+import GuiTheme from '../../../app/gui-theme';
 
 const styles = theme => ({
     propertyInput: {
